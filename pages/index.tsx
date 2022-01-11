d78ff908a7f99d92d5882ddb25c8d1aba2230489
Yama-Souichirou <souichirou19960210@gmail.com>
2022-01-11 15:28:09 +0900

    path を変える

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Home: NextPage = () => {
   return (
     <div>
-      <Link href="/samples/useeffect">
+      <Link href="/hooks/useeffect">
         <a>useEffect</a>
       </Link>
     </div>
