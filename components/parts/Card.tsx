3bb355eae61391cc23d99c7cda43eb9ff6585eb2
Yama-Souichirou <souichirou19960210@gmail.com>
2022-01-15 20:32:23 +0900

    カード UI にタイトルを付けられるようにする

--- components/parts/Card.tsx
+++ components/parts/Card.tsx
@@ -1,18 +1,30 @@
+import { VFC, PropsWithChildren } from "react"
 import {
   Box,
   useColorModeValue,
+  Heading,
+  Text
 } from '@chakra-ui/react';
 
-export function Card({ children }: { children: ReactNode }) {
-return (
-  <Box
-    w="100%"
-    borderRadius="lg"
-    direction={{ base: 'column', md: 'row' }}
-    bg={useColorModeValue('white', 'gray.900')}
-    boxShadow={'2xl'}
-    padding={4}>
-    {children}
-  </Box>
+type Props = {
+  title?: string
+  description?: string
+}
+
+export const Card: VFC<PropsWithChildren<Props>> = ({ children, title, description }) => {
+  return (
+    <Box
+      w="100%"
+      borderRadius="lg"
+      direction={{ base: 'column', md: 'row' }}
+      bg={useColorModeValue('white', 'gray.900')}
+      boxShadow={'2xl'}
+      padding={4}>
+      <>
+        <Heading as="h3">{title}</Heading>
+        <Text>{description}</Text>
+        {children}
+      </>
+    </Box>
   );
 }
