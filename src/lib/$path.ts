4a63ebc69942e2458463954ac1c5a84fc3764b3f
Ya-masou <s.yama@ashita-team.com>
2022-03-18 08:48:28 +0900

    pathpida の output 先を変更する

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -5,6 +5,9 @@ export const pagesPath = {
     customhooks: {
       $url: (url?: { hash?: string }) => ({ pathname: '/hooks/customhooks' as const, hash: url?.hash })
     },
+    usecontext: {
+      $url: (url?: { hash?: string }) => ({ pathname: '/hooks/usecontext' as const, hash: url?.hash })
+    },
     useeffect: {
       $url: (url?: { hash?: string }) => ({ pathname: '/hooks/useeffect' as const, hash: url?.hash })
     },
@@ -13,6 +16,11 @@ export const pagesPath = {
     },
     usestate: {
       $url: (url?: { hash?: string }) => ({ pathname: '/hooks/usestate' as const, hash: url?.hash })
+    }
+  },
+  uis: {
+    tables: {
+      $url: (url?: { hash?: string }) => ({ pathname: '/uis/tables' as const, hash: url?.hash })
     }
   },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
