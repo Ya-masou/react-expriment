3bb1a3ae5054fbd50c57826d321bbb7ee1ec68ad
Ya-masou <s.yama@ashita-team.com>
2022-04-03 18:24:54 +0900

    eslint 対象外ファイルを定義する

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  ignorePatterns: ["src/generated/*"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
